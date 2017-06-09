e3383fe5825a1baa0da723fbdc198c2c5d291260
Andrii_Savka <Andrii_Savka@EPUALVIW0529.kyiv.epam.com>
2017-06-09 13:56:48 +0300

    delete custom validation messages from WeatherObjectDto

--- weatherStorageService/src/main/java/mteo/savand/dto/WeatherObjectDto.java
+++ weatherStorageService/src/main/java/mteo/savand/dto/WeatherObjectDto.java
@@ -11,37 +11,38 @@ import org.joda.time.DateTime;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 
-@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE,
+        isGetterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WeatherObjectDto {
-    @NotNull(message = "stationId must not be null")
+    @NotNull
     private Integer stationId;
 
-    @NotNull(message = "locationLatitude must not be null")
+    @NotNull
     private Float locationLatitude;
 
-    @NotNull(message = "locationLongitude must not be null")
+    @NotNull
     private Float locationLongitude;
 
-    @NotNull(message = "date must not be null")
+    @NotNull
     private DateTime date;
 
-    @NotNull(message = "temperatureInDegreesCelsious must not be null")
-    @Range(min = -100, max = 60, message = "temperatureInDegreesCelsious sould be in the range -100 - 60")
+    @NotNull
+    @Range(min = -100, max = 60)
     private Integer temperatureInDegreesCelsious;
 
-    @NotNull(message = "humidityInPercentage must not be null")
-    @Range(min = 0, max = 100, message = "Percentage field should be between 0-100")
+    @NotNull
+    @Range(min = 0, max = 100)
     private Integer humidityInPercentage;
 
-    @NotNull(message = "windSpeedInKnots must not be null")
-    @Range(min = 0, max = 220, message = "windSpeedInKnots must be in range 0-220")
+    @NotNull
+    @Range(min = 0, max = 220)
     private Integer windSpeedInKnots;
 
-    @NotBlank(message = "windDirection must not be null")
+    @NotBlank
     private String windDirection;
 
-    
+
     public Integer getStationId() {
         return stationId;
     }
@@ -105,11 +106,11 @@ public class WeatherObjectDto {
     public void setWindDirection(String windDirection) {
         this.windDirection = windDirection;
     }
-    
+
     public static WeatherObjectDtoBuilder newBuilder() {
         return new WeatherObjectDto().new WeatherObjectDtoBuilder();
     }
-    
+
 
     public class WeatherObjectDtoBuilder {
 
@@ -124,39 +125,40 @@ public class WeatherObjectDto {
             WeatherObjectDto.this.locationLatitude = locationLatitude;
             return this;
         }
-        
+
         public WeatherObjectDtoBuilder setLocationLongitude(float locationLongitude) {
             WeatherObjectDto.this.locationLongitude = locationLongitude;
             return this;
         }
-        
+
         public WeatherObjectDtoBuilder setDate(DateTime date) {
             WeatherObjectDto.this.date = date;
             return this;
         }
-        
-        public WeatherObjectDtoBuilder setTemperatureInDegreesCelsious(int temperatureInDegreesCelsious) {
+
+        public WeatherObjectDtoBuilder setTemperatureInDegreesCelsious(
+                int temperatureInDegreesCelsious) {
             WeatherObjectDto.this.temperatureInDegreesCelsious = temperatureInDegreesCelsious;
             return this;
         }
-        
-        
+
+
         public WeatherObjectDtoBuilder setHumidityInPercentage(int humidityInPercentage) {
             WeatherObjectDto.this.humidityInPercentage = humidityInPercentage;
             return this;
         }
-        
+
         public WeatherObjectDtoBuilder setWindSpeedInKnots(int windSpeedInKnots) {
             WeatherObjectDto.this.windSpeedInKnots = windSpeedInKnots;
             return this;
         }
-        
+
         public WeatherObjectDtoBuilder setWindDirection(String windDirection) {
             WeatherObjectDto.this.windDirection = windDirection;
             return this;
         }
-        
-        
+
+
         public WeatherObjectDto build() {
             return WeatherObjectDto.this;
         }
@@ -241,7 +243,7 @@ public class WeatherObjectDto {
                 + ", humidityInPercentage=" + humidityInPercentage + ", windSpeedInKnots="
                 + windSpeedInKnots + ", windDirection=" + windDirection + "]";
     }
-    
-    
+
+
 
 }
